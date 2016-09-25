981bcabeb0d9a9179f18480c16c7bed25a666882
cosven <yinshaowen241@gmail.com>
2016-09-25 13:19:08 +0800

    add `playlist` `playback mode` concept to Player

--- fuocore/player.py
+++ fuocore/player.py
@@ -2,10 +2,12 @@
 
 from enum import Enum
 import logging
-import threading
+import random
 import subprocess
+import threading
 
 from .dispatch import Signal
+from .base_model import SongModel
 
 
 class State(Enum):
@@ -14,11 +16,29 @@ class State(Enum):
     playing = 2
 
 
+class PlaybackMode(Enum):
+    one_loop = 0
+    sequential = 1
+    loop = 2
+    random = 3
+
+
+class _SimpleSongModel(SongModel):
+    def __init__(self, url):
+        self._url = url
+
+    @property
+    def url(self):
+        return self._url
+
+
 class Player(object):
     '''mpg123 wrapper
 
     Each time the player play a song, it will kill the old process and open
     a new subprocess.
+
+    TODO: maybe we need a `Playlist` class
     '''
     def __init__(self):
         super().__init__()
@@ -28,11 +48,21 @@ class Player(object):
 
         self._position = 0
         self._state = State.stopped
+        self._last_state = self._state
         self._duration = 0
+        self._playback_mode = PlaybackMode.loop
+        self._last_playback_mode = self._playback_mode
+
+        # no repetition but ordering
+        self._songs = []
+        # current_song is None or `SongModel` instance and must in `_songs` list
+        self._current_song = None
 
         self.finished = Signal()
         self.position_changed = Signal()
         self.state_changed = Signal()
+
+        self.finished.connect(self.__play_next)
 
     @property
     def handler(self):
@@ -61,24 +91,23 @@ class Player(object):
                 self._position = float(sec)
                 if int(framecount) == 0:
                     self._duration = float(sec_left)
-                self._state = State.playing
+                self.set_state(State.playing)
                 self.position_changed.emit()
             elif flag == '@P':
                 a = line[3:4]
                 if a == '0':
                     logging.info('Player stopped.')
-                    self._state = State.stopped
+                    self.set_state(State.stopped)
                     self._duration = 0
                     self._position = 0
                     self.finished.emit()
                     break
                 elif a == '1':
                     logging.info('Player paused.')
-                    self._state = State.paused
+                    self.set_state(State.paused)
                 else:
                     logging.info('Player resumed.')
-                    self._state = State.playing
-                self.state_changed.emit()
+                    self.set_state(State.playing)
             elif flag == '@E':
                 logging.warning('Player error occured.')
 
@@ -111,6 +140,18 @@ class Player(object):
     def state(self):
         return self._state
 
+    @property
+    def playback_mode(self):
+        return self._playback_mode
+
+    @property
+    def current_song(self):
+        return self._current_song
+
+    @property
+    def songs(self):
+        return self._songs
+
     def play(self):
         if self.state == State.paused:
             self.toggle()
@@ -130,8 +171,90 @@ class Player(object):
     def stop(self):
         self.destroy()
 
-    def play_song(self, url):
+    def set_state(self, state):
+        self._state = state
+        if self._last_state != state:
+            self.state_changed.emit()
+            self._last_state = state
+
+    def _play(self, url):
         self.destroy()
         cmd = 'L {0}\n'.format(url)
         self.handler.stdin.write(cmd.encode('utf8'))
         self.handler.stdin.flush()
+
+    def play_song(self, song):
+        self._play(song.url)
+        self._current_song = song
+        self.add_song(song)
+
+    def play_songs(self, songs):
+        self._songs = songs
+        if self._songs:
+            self.play_song(self._songs[0])
+
+    def _get_song(self, song):
+        '''get song instance in `_songs` list'''
+        for i, m in enumerate(self._songs):
+            if song.mid == m.mid:
+                return song
+        return None
+
+    def add_song(self, song, insert=False):
+        if self._get_song(song) is None:
+            if insert:
+                if self._current_song is None:
+                    index = 0
+                else:
+                    current_index = self._songs.index(self._current_song)
+                    index = current_index + 1
+                self._songs.insert(index, song)
+            else:
+                self._songs.append(song)
+            return True
+        return False
+
+    def remove_song(self, song):
+        song = self._get_song(song)
+        if song is None:
+            return None
+        self._songs.remove(song)
+        return True
+
+    def get_previous_song(self):
+        if not self._songs:
+            return None
+        if self.playback_mode == PlaybackMode.one_loop:
+            return self._current_song
+        elif self.playback_mode == PlaybackMode.loop:
+            current_index = self._songs.index(self._current_song)
+            if current_index == 0:
+                return self._songs[-1]
+            else:
+                return current_index - 1
+        elif self.playback_mode == PlaybackMode.sequential:
+            return None
+        else:
+            return random.choice(self._songs)
+
+    def get_next_song(self):
+        if not self._songs:
+            return None
+        if self.playback_mode == PlaybackMode.one_loop:
+            return self._current_song
+        elif self.playback_mode == PlaybackMode.loop:
+            if self._current_song == self._songs[-1]:
+                return self._songs[0]
+            else:
+                return self._songs[self._songs.index(self._current_song) + 1]
+        elif self.playback_mode == PlaybackMode.sequential:
+            self.signal_playlist_finished.emit()
+            return None
+        else:
+            return random.choice(self._songs)
+
+    def __play_next(self):
+        '''on song finished callback'''
+        song = self.get_next_song()
+        if song is not None:
+            self.play_song(song)
--- fuocore/test/test_player.py
+++ fuocore/test/test_player.py
@@ -1,10 +1,28 @@
 import os
-from unittest import TestCase
+import time
+from unittest import TestCase, mock
 
 from fuocore.player import Player, State
-
+from fuocore.base_model import SongModel
 
 MP3_URL = os.path.join(os.path.dirname(__file__), 'fixtures', 'ybwm-ts.mp3') 
+
+
+class FakeSongModel(SongModel):
+    def __init__(self):
+        pass
+
+    @property
+    def mid(self):
+        return '00000000'
+
+    @property
+    def title(self):
+        return 'fake-title'
+
+    @property
+    def url(self):
+        return MP3_URL
 
 
 class PlayerTest(TestCase):
@@ -21,14 +39,39 @@ class PlayerTest(TestCase):
         self.player.play()
 
     def test_pause(self):
-        self.player.play_song(MP3_URL)
+        self.player._play(MP3_URL)
         self.player.pause()
 
     def test_play_song(self):
-        self.player.play_song('...')
+        song = FakeSongModel()
+        self.player.play_song(song)
+        self.assertTrue(self.player.current_song == song)
+        time.sleep(1)
+        self.assertTrue(self.player.state == State.playing)
+
+    @mock.patch.object(Player, 'play_song')
+    def test_play_songs(self, mock_play_song):
+        song1 = FakeSongModel()
+        song2 = FakeSongModel()
+        self.player.play_songs([song1, song2])
+        self.assertTrue(mock_play_song.called)
 
     def test_toggle(self):
         self.player.toggle()
 
     def test_position(self):
         self.player.position
+
+    def test_add_song(self):
+        song = FakeSongModel()
+        self.player.add_song(song)
+        self.assertTrue(len(self.player.songs), 1)
+
+    def test_get_next_song(self):
+        song1 = FakeSongModel()
+        song2 = FakeSongModel()
+        self.player.play_songs([song1, song2])
+        self.assertTrue(self.player.get_next_song() == song2)
+
+    def test_get_previous_song(self):
+        pass
