09c923395717cc48854268398c00d7d73158651b
ysw <yinshaowen241@gmail.com>
2017-05-31 22:04:46 +0800

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = [
 
 setup(
     name='fuocore',
-    version='0.0.5a2',
+    version='0.0.5a3',
     description='feeluown core',
     author='Cosven',
     author_email='cosven.yin@gmail.com',
