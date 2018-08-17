f956545c168fceffcddc1045bcc96783dcd73c27
cosven <yinshaowen241@gmail.com>
2018-08-17 16:06:04 +0800

    add xiami provider

---
+++ fuocore/xiami/provider.py
@@ -0,0 +1,44 @@
+import logging
+
+from contextlib import contextmanager
+from fuocore.provider import AbstractProvider
+from fuocore.xiami.api import API
+
+
+logger = logging.getLogger(__name__)
+
+
+class XiamiProvider(AbstractProvider):
+    def __init__(self):
+        self.api = API()
+
+        self._user = None
+
+    @property
+    def identifier(self):
+        return 'xiami'
+
+    @property
+    def name(self):
+        return '虾米音乐'
+
+    @contextmanager
+    def auth_as(self, user):
+        old_user = self._user
+        self.auth(user)
+        try:
+            yield
+        finally:
+            self.auth(old_user)
+
+    def auth(self, user):
+        assert user.cookies is not None
+        self._user = user
+        self.api.load_cookies(user.cookies)
+
+
+provider = XiamiProvider()
+
+from .models import search  # noqa
+
+provider.search = search
