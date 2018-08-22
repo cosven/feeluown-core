fbae88d4c6a257e72da03241393dcb6b4261f2b8
cosven <yinshaowen241@gmail.com>
2018-08-22 11:39:04 +0800

    update docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
             return MagicMock()
 
 
-MOCK_MODULES = ['cfbclient', 'pstatsd']
+MOCK_MODULES = ['mpv']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('../..'))
